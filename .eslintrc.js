0eb0055f2c90ce6e73ea18c62847bff6250d8ad0
Justin <JustinKruskie@gmail.com>
2020-04-11 04:10:18 -0400

    ESLint Init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,9 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+  'extends': [
+    'plugin:vue/essential',
+    'google',
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -17,11 +14,10 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+  'plugins': [
+    'vue',
+    '@typescript-eslint',
+  ],
       ],
       env: {
         jest: true
