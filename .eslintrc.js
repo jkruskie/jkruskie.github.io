9c6c254a6127f94077ebe70c1b9c33e9bf2605c0
Justin <JustinKruskie@gmail.com>
2020-04-11 04:10:18 -0400

    Revert "ESLint Init"
    This reverts commit 0eb0055f2c90ce6e73ea18c62847bff6250d8ad0.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'google',
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -14,10 +17,11 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
-  'plugins': [
-    'vue',
-    '@typescript-eslint',
-  ],
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         jest: true
