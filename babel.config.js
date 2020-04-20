566feacfd11e3c61f6a6f71c7bee714379830e98
Justin <JustinKruskie@gmail.com>
2020-04-19 23:04:41 -0400

    Update Icon

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
+// module.exports = {
+//   presets: ["@vue/cli-plugin-babel/preset"]
+// };
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: [
+    ['@vue/babel-preset-app', { modules: false }],
+  ],
+  plugins: [
+    'dynamic-import-node',
+  ],
+  env: {
+    test: {
+      presets: [
+        ['@vue/babel-preset-app', { modules: 'commonjs', useBuiltIns: false }],
+      ],
+    },
+  },
 };
